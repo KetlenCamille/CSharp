78e726e5addcc24f3947b2d1301550b5669dfde7
KetlenCamille <ketlen.rieper@gmail.com>
2018-05-10 09:38:27 -0300

    Criando e utilizando classe Cliente, juntamente com seus atributos e propriedades

---
+++ Aulas/Aula1005_Console/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1005_Console
+{
+    class Cliente
+    {
+        //Atributo
+        private string nome;
+
+        //Propriedade
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+        //Atributo e Propriedade junto
+        public string Cpf { get; set; }
+    }
+}
---
+++ Aulas/Aula1005_Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1005_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+            cliente.Nome = "Godofredo"; //Set
+            string nomeCliente = cliente.Nome; //Get
+
+            cliente.Cpf = "111.222.333-44"; //Set
+            string cpfCliente = cliente.Cpf; //Get
+
+            Console.WriteLine(nomeCliente);
+        }
+    }
+}
