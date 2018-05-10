41285ef8a7776747db1c638b5cd6466000ff0df4
KetlenCamille <ketlen.rieper@gmail.com>
2018-05-10 10:34:01 -0300

    Migrando informações de Cliente para Pessoa

--- Aulas/Aula1005_Console/Cliente.cs
+++ Aulas/Aula1005_Console/Cliente.cs
@@ -8,40 +8,6 @@ namespace Aula1005_Console
 {
     class Cliente
     {
-        //Atributo
-        private string nome;
-
-        //Propriedade
-        public string Nome
-        {
-            get
-            {
-                return nome;
-            }
-
-            set {
-                nome = value;
-            }
-        }
-
-        //Atributo e Propriedade junto
-        public string Cpf { get; set; }
-
-        //Constructor
-        public Cliente(String nome)
-        {
-            Nome = nome;
-        }
-
-        public Cliente(String nome, String cpf)
-        {
-            Nome = nome;
-            Cpf = cpf;
-        }
-        //Constructor Vazio
-        public Cliente()
-        {
-
-        }
+        
     }
 }
---
+++ Aulas/Aula1005_Console/Pessoa.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1005_Console
+{
+    class Pessoa
+    {
+        //Atributo
+        private string nome;
+
+        //Propriedade
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        //Atributo e Propriedade junto
+        public string Cpf { get; set; }
+
+        //Constructor
+        public Pessoa(String nome)
+        {
+            Nome = nome;
+        }
+
+        public Pessoa(String nome, String cpf)
+        {
+            Nome = nome;
+            Cpf = cpf;
+        }
+        //Constructor Vazio
+        public Pessoa()
+        {
+
+        }
+
+    }
+}
