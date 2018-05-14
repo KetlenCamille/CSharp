bfbaa5497fb3427fd809512c6300cb98b4f620ab
KetlenCamille <ketlen.rieper@gmail.com>
2018-05-14 11:27:36 -0300

    Criando rodas via for

--- Aulas/Aula1405_Interfaces/Program.cs
+++ Aulas/Aula1405_Interfaces/Program.cs
@@ -36,7 +36,8 @@ namespace Aula1405_Interfaces
 
             AcelerarBastante(onibus);
             Console.WriteLine(onibus.ImprimirInfo());
-           
+
+            CriarRelacionamentos();
             Console.ReadKey();
         }
         //Injeção de dependência
@@ -55,8 +56,20 @@ namespace Aula1405_Interfaces
 
             Marca m = new Marca();
             m.Nome = "Chevrolet";
-
+            //Atribuindo marca
             c._Marca = m;
+
+            //Criando e armazenando rodas
+            c.Rodas = new List<Roda>();
+            Roda r1 = new Roda();
+            c.Rodas.Add(r1);
+           
+            for(int i = 0; i < 3; i++)
+            {
+                c.Rodas.Add(new Roda());
+            }
+
+            Console.WriteLine("Qtde rodas: " + c.Rodas.Count);
 
             Onibus o = new Onibus();
             o.Modelo = "3100";
