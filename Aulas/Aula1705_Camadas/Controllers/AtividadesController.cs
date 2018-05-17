712c6bee04b4dfb6a7dde1a4a7aff825d96bc77b
KetlenCamille <ketlen.rieper@gmail.com>
2018-05-17 08:48:50 -0300

    Criando model, view e controller e método salvar

---
+++ Aulas/Aula1705_Camadas/Controllers/AtividadesController.cs
@@ -0,0 +1,19 @@
+﻿using Aula1705_Camadas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1705_Camadas.Controllers
+{
+    class AtividadesController
+    {
+        public List<Atividade> ListaAtividades { get; set; }
+
+        public void salvarAtividade(Atividade atividade)
+        {
+            ListaAtividades.Add(atividade);
+        }
+    }
+}
