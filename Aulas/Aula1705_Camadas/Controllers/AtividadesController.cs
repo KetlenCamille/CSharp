64610677380ad5e02070aa40e0e29220324bb98b
KetlenCamille <ketlen.rieper@gmail.com>
2018-05-17 08:57:17 -0300

    Criando método BuscarPorID

--- Aulas/Aula1705_Camadas/Controllers/AtividadesController.cs
+++ Aulas/Aula1705_Camadas/Controllers/AtividadesController.cs
@@ -10,15 +10,30 @@ namespace Aula1705_Camadas.Controllers
     class AtividadesController
     {
         public List<Atividade> ListaAtividades { get; set; }
-
+        
+        //Salvar
         public void salvarAtividade(Atividade atividade)
         {
             ListaAtividades.Add(atividade);
         }
 
+        //Listar
         public List<Atividade> listarAtividades()
         {
             return ListaAtividades;
+        }
+
+        //Buscar
+        public Atividade buscarAtividadePorID (int AtividadeID)
+        {
+            foreach (Atividade atividade in ListaAtividades)
+            {
+                if(atividade.AtividadeID == AtividadeID)
+                {
+                    return atividade;
+                }
+            }
+            return null;
         }
 
     }
