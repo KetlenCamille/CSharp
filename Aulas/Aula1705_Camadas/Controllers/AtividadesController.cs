d6227190ab9dd919f0360ad71ff60adcbf070c76
KetlenCamille <ketlen.rieper@gmail.com>
2018-05-21 08:19:32 -0300

    ID sendo calculado corretamente

--- Aulas/Aula1705_Camadas/Controllers/AtividadesController.cs
+++ Aulas/Aula1705_Camadas/Controllers/AtividadesController.cs
@@ -11,6 +11,8 @@ namespace Aula1705_Camadas.Controllers
     {
         private static List<Atividade> ListaAtividades { get; set; }
 
+        static int id = 0;
+
         static AtividadesController()
         {
             ListaAtividades = new List<Atividade>();
@@ -19,7 +21,8 @@ namespace Aula1705_Camadas.Controllers
         //Salvar
         public void salvarAtividade(Atividade atividade)
         {
-            atividade.AtividadeID = ListaAtividades.Count + 1;
+            atividade.AtividadeID = ++id;
+            //atividade.AtividadeID = ListaAtividades.Count + 1;
             ListaAtividades.Add(atividade);
         }
 
