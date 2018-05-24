f9639ea4548a32c6945cd08d7e7803e5394d6d46
KetlenCamille <ketlen.rieper@gmail.com>
2018-05-24 11:26:55 -0300

    Implentando interface na classe Categoria

--- ControleAtividades/Controllers/CategoriaControllers.cs
+++ ControleAtividades/Controllers/CategoriaControllers.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Controllers.Base;
+using Modelos;
 
 namespace Controllers
 {
-    class CategoriaControllers
+    class CategoriaControllers : IBaseController<Categoria>
     {
+        public static List<Categoria> {get; set;}
+
+        public void Adicionar(Categoria entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(int id, Categoria entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Categoria BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Categoria> ListarPorNome(string nome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Categoria> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
