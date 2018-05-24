d8f6359d36d0760f4030d66b43f3e784e8379d31
KetlenCamille <ketlen.rieper@gmail.com>
2018-05-24 10:34:29 -0300

    Criando o método SalvarUsuario

--- ControleAtividades/Controllers/UsuarioControllers.cs
+++ ControleAtividades/Controllers/UsuarioControllers.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace Controllers
 {
     class UsuarioControllers
     {
+        public List<Usuario> ListaUsuarios { get; set; }
+
+        public void SalvarUsuario (Usuario usuario)
+        {
+            if(usuario != null)
+            {
+                ListaUsuarios.Add(usuario);
+            }
+        }
+
     }
 }
