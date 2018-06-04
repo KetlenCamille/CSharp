1e89a3d0b001abc35f70a8b05ea162bf9a1ad208
KetlenCamille <ketlen.rieper@gmail.com>
2018-06-04 09:06:37 -0300

    Criando método Adiocionar( ) na AtividadeControllers

--- ControleAtividades/Controllers/AtividadeControllers.cs
+++ ControleAtividades/Controllers/AtividadeControllers.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Controllers.Base;
+using Controllers.DAL;
+using Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,42 @@ using System.Threading.Tasks;
 
 namespace Controllers
 {
-    public class AtividadeControllers
+    public class AtividadeControllers : IBaseController<Atividade>
     {
+        private Contexto contexto = new Contexto();
+
+        public void Adicionar(Atividade atividade)
+        {
+            if (atividade != null)
+            {
+                contexto.Atividades.Add(atividade);
+                contexto.SaveChanges();
+            }
+        }
+
+        public void Atualizar(int id, Atividade entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Atividade BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Atividade> ListarPorNome(string nome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Atividade> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
